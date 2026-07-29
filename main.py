669a3b76beeb99b93816062ea9f404d92228bfd4
Camilo Cortes <cortesvargascamiloandres@gmail.com>
2026-07-28 21:41:47 -0500

    The snake now moves smoothly and doesn't just stretch

--- main.py
+++ main.py
@@ -25,59 +25,68 @@ class Fruit:
     def draw_fruit(self):
         self.rect = pygame.Rect(self.pos.x*CELL_SIZE, self.pos.y*CELL_SIZE, CELL_SIZE, CELL_SIZE)
         pygame.draw.rect(WIN, fruit_color, self.rect)
-        pygame.display.update()
 
 
 class Snake:
     def __init__(self):
-        self.body = [Vector2(7,10), Vector2(6,10), Vector2(5,10)]
-        self.direction = Vector2(1,0)
+        self.body = [Vector2(7*CELL_SIZE,10*CELL_SIZE), Vector2(6*CELL_SIZE,10*CELL_SIZE), Vector2(5*CELL_SIZE,10*CELL_SIZE)]
+        self.direction = Vector2(5,0)
         
     def draw_snake(self):
         for block in self.body:
-            body_block = pygame.Rect(block.x*CELL_SIZE, block.y*CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            body_block = pygame.Rect(block.x, block.y, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(WIN, snake_color, body_block)
 
     def move(self):
         for i, block in enumerate(self.body):
-            self.body[i] = block + self.direction
-        self.draw_snake()
+            self.body[i] = block + self.direction      
+        #self.draw_snake()
         
-        
+# Creating the background        
+def set_background():
 
-# Creating the background
-even_iteration = True # This is a variable to apply a color in one iteration and another one in the next one, between 2 colors
-for row in range(WIDTH//CELL_SIZE):
-    for column in range(HEIGHT//CELL_SIZE):
-        slab_pos = Vector2(row*CELL_SIZE, column*CELL_SIZE)
-        slab = pygame.Rect(slab_pos.x, slab_pos.y, CELL_SIZE, CELL_SIZE)
+    even_iteration = True # This is a variable to apply a color in one iteration and another one in the next one, between 2 colors
+
+    for row in range(WIDTH//CELL_SIZE):
+        for column in range(HEIGHT//CELL_SIZE):
+            slab_pos = Vector2(row*CELL_SIZE, column*CELL_SIZE)
+            slab = pygame.Rect(slab_pos.x, slab_pos.y, CELL_SIZE, CELL_SIZE)
+            if even_iteration:
+                pygame.draw.rect(WIN, darker_green, slab)
+                even_iteration = False
+            else:
+                pygame.draw.rect(WIN, lighter_green, slab)
+                even_iteration = True
+        
         if even_iteration:
-            pygame.draw.rect(WIN, darker_green, slab)
             even_iteration = False
         else:
-            pygame.draw.rect(WIN, lighter_green, slab)
             even_iteration = True
-    
-    if even_iteration:
-        even_iteration = False
-    else:
-        even_iteration = True
 
 # Creating the fruit
 fruit = Fruit()
-fruit.draw_fruit()
 
 # Creating the snake
 snake = Snake()
+
+
+set_background()
 snake.draw_snake()
+started = False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        
+        elif event.type == pygame.KEYDOWN:
+            started = True
     
-    snake.move()
+    set_background()
+    fruit.draw_fruit()
+    if started: snake.move() 
+    snake.draw_snake()
     pygame.display.update()
 
-    clock.tick(30)
+    clock.tick(60)
